af4e9e6d8811a9f554b8d168838914b8559e2a11
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-27 07:52:38 +0530

    Added logic for creating a Tic Tac Toe board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,22 @@
 class TicTacToe
 {
+	static char[] board;
+
+	public void printboard()
+	{
+		for(int i=1;i<10;i++)
+		{
+			board[i]=' ';
+			System.out.println(board[i]);
+		}
+	}
+
     public static void main(String[] args)
    {
+		TicTacToe tictac=new TicTacToe();
+		board= new char[10];
 
+		tictac.printboard();
    }
 }
 
