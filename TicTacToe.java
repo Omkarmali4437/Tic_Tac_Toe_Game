9b3ea12452f5f4af02c9d30a7eead8ec0c280b10
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-27 21:08:16 +0530

    Added logic for showing a tic tac toe board grid

--- TicTacToe.java
+++ TicTacToe.java
@@ -35,7 +35,16 @@ class TicTacToe
 		}
 	}
 
-    public static void main(String[] args)
+	public void showboard()
+	{
+		System.out.println(board[1] + " | "+ board[2] + " | " +  board[3]);
+		System.out.println("--------");
+		System.out.println(board[4] + " | "+ board[5] + " | " +  board[6]);
+		System.out.println("--------");
+		System.out.println(board[7] + " | "+ board[8] + " | " +  board[9]);
+	}
+
+	public static void main(String[] args)
    {
 		TicTacToe tictac=new TicTacToe();
 		board= new char[10];
@@ -46,8 +55,7 @@ class TicTacToe
 		tictac.assign(symbol);
 		System.out.println("Player is: "+player);
 		System.out.println("Computer is: "+computer);
-
-		tictac.printboard();
+		tictac.showboard();
    }
 }
 
