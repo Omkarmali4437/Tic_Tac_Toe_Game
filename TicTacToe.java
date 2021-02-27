906f91c329a39ad408389e9ea130d0453c7d156b
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-27 08:00:59 +0530

    Added logic for choosing a player and computer symbol

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 class TicTacToe
 {
 	static char[] board;
+	public static String computer;
+	public static String player;
+
+	public static String assign(String symbol)
+	{
+		if(symbol.equals("X"))
+		{
+			player="X";
+			computer="O";
+		}
+		else if(symbol.equals("O"))
+		{
+			player="O";
+			computer="X";
+		}
+		else
+		{
+			System.out.println("Wrong input");
+		}
+		return computer;
+
+	}
 
 	public void printboard()
 	{
@@ -15,6 +39,13 @@ class TicTacToe
    {
 		TicTacToe tictac=new TicTacToe();
 		board= new char[10];
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your choice X|O");
+		String symbol=sc.next();
+		tictac.assign(symbol);
+		System.out.println("Player is: "+player);
+		System.out.println("Computer is: "+computer);
 
 		tictac.printboard();
    }
