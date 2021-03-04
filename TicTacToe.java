e9a3999193df82a3fa85a81cec213610f3a944fa
omkarmali4437 <omkarmalli688@gmail.com>
2021-03-04 11:43:27 +0530

    Added logic for taking centre if available not then take any sides

--- TicTacToe.java
+++ TicTacToe.java
@@ -190,6 +190,8 @@ public class TicTacToe {
 	
 	public static void computerMove() {
 		int[] corner = {1, 3, 7, 9};
+		int centre = 5;
+		int[] sides = {2, 4, 6, 8};
 
 			boolean check = true;
 				for (int l=0; l<corner.length; l++) {
@@ -199,7 +201,24 @@ public class TicTacToe {
 						break;
 					}
 				}
+					
+					if (check) {
+						if (board[centre] == ' ') {
+							board[centre] = ComputerLetter;
+							check = false;
+						}
+					}
+					
+					if (check) {
+						for(int l=0; l<sides.length; l++) {
+							if (board[sides[l]] == ' ') {
+								board[sides[l]] = ComputerLetter;
+								break;
+							}
+						}
+					}
 	}
+	
 	
 	public static void makeComputerMove() {
 		if (computerWinning()) {
